7aebc281ad2f4c039d11ca15a7d27cf5d1f17c73
Astrid773 <sanchez_lezama10@hotmail.com>
2025-07-03 10:50:47 -0600

    Add files via upload

---
+++ Areas/Identity/Data/JunjiItoContext.cs
@@ -0,0 +1,22 @@
+﻿using JunjiIto.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JunjiIto.Areas.Identity.Data;
+
+public class JunjiItoContext : IdentityDbContext<JunjiItoUser>
+{
+    public JunjiItoContext(DbContextOptions<JunjiItoContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
