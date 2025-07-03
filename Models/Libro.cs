93936964a0dc181226cc1e8894eab416183e6520
Astrid773 <sanchez_lezama10@hotmail.com>
2025-07-03 10:49:48 -0600

    Add files via upload

---
+++ Models/Libro.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JunjiIto.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre de obra")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Descripción de obra")]
+        public string Descripcion {  get; set; }
+
+        [Display(Name = "Año de publicación")]
+        [DataType(DataType.Date)]
+        public string Publicacion { get; set; }
+
+        public string FotoLibro { get; set; }
+    }
+}
