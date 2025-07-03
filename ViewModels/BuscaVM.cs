0e9acb558f4a1061a0f514bdce0880e11676d913
Astrid773 <sanchez_lezama10@hotmail.com>
2025-07-03 10:49:37 -0600

    Add files via upload

---
+++ ViewModels/BuscaVM.cs
@@ -0,0 +1,13 @@
+﻿using JunjiIto.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace JunjiIto.ViewModels
+{
+    public class BuscaVM
+    {
+        public List<Libro> LosLibros { get; set; }
+        
+        public string BNom {  get; set; }
+        public string BPublicacion { get; set; }
+    }
+}
---
+++ ViewModels/LibroVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JunjiIto.ViewModels
+{
+    public class LibroVM
+    {
+        [Required]
+        [Display(Name = "Nombre de obra")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Descripción de obra")]
+        public string Descripcion { get; set; }
+
+        [Display(Name = "Año de publicación")]
+        [DataType(DataType.Date)]
+        public string Publicacion { get; set; }
+
+        public IFormFile Foto { get; set; }
+    }
+}
